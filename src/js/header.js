a2f4511989f2ec8c90b030fc23ff722fd0453b9a
nekonisi <konishi2175@gmail.com>
2018-12-09 20:28:14 +0900

    ヘッダの更新

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,8 @@
-document.write(' <div>header</div> <div>header2</div> ');
+header=
+	'<head>'
+	'<meta charset="UTF-8">'
+	'<meta name="viewpoint" content="width=device-width, initial-scale=1">'
+	'<title>個人用サイト</title>'
+	'</head>';
+
+document.write(header);
