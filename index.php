4fba1fd19e5de441fe6efd6f554479105cf856fe
nekonisi <konishi2175@gmail.com>
2018-12-10 22:07:38 +0900

    ナビゲーションバーの修正

--- index.php
+++ index.php
@@ -18,13 +18,24 @@
 		<!-- ナビゲーションバーここから-->
 <nav class="navbar navbar-inverse">
 	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarEexample7">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">
+				タイトル
+			</a>
+		</div>
+
 		<div class="collapse navbar-collapse" id="navbarEexample7">
 			<ul class="nav navbar-nav">
-						<li class="active"><a href="/">About</a></li>
-						<li><a href="/php/">PHPに関して</a></li>
-						<li><a href="/designPattern/">デザインパターンに関して</a></li>
+				<li class="active"><a href="/">about</a></li>
+				<li><a href="/php/">PHP</a></li>
+				<li><a href="/DesignPattern/">DesignPattern</a></li>
 			</ul>
-			<p class="navbar-text navbar-right">ようこそ <a href="#" class="navbar-link">ゲスト</a> さん。</p>
 		</div>
 	</div>
 </nav>
