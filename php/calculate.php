c219278c9ad7ee3c0d3611cdf9cbeb3d25043945
nekonisi <konishi2175@gmail.com>
2018-12-11 12:56:26 +0900

    phpのコード紹介ページのテスト

--- php/calculate.php
+++ php/calculate.php
@@ -44,13 +44,35 @@
 	</header>
 	<body>
 <div class="contaigner">
-<h2>コード</h2>
-<code>
-$price = 12300;<br>
-$total = $price * 1.05;
-</code>
+
+<!--概要説明-->
+<div class="panel panel-default">
+	<div class="panel-heading">
+		概要
+	</div>
+	<div class="panel-body">
+		変数を使って，PHPによる変数の出力テストを行う。
+	</div>
 </div>
-<!--PHPのコード-->
+<!--概要説明-->
+
+<!--サンプルコード-->
+<div class="panel panel-default">
+	<div class="panel-heading">
+		サンプルコード
+	</div>
+	<div class="panel-body">
+<code>
+<pre>
+$price = 12300;
+$total = $price * 1.05;
+</pre>
+</code>
+	</div>
+</div>
+<!--サンプルコード-->
+
+<!--PHPの実コード-->
 <?php
 $price = 12300;
 $total = $price * 1.05;
@@ -58,10 +80,14 @@ $total = $price * 1.05;
 <!--PHPのコード-->
 
 <!--出力結果-->
-<div class="contaigner">
-<h2>出力結果</h2>
+<div class="panel panel-default">
+	<div class="panel-heading">
+		出力結果
+	</div>
+	<div class="panel-body">
 	<p><?php echo '$price = '.$price; ?></p>
 	<p><?php echo '$total = '.$total; ?></p>
+	</div>
 </div>
 <!--出力結果-->
 	</body>
